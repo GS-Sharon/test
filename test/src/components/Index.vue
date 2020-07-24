4c19779705bc059d1df46cfd8e81ac704c735cd7
shunhua.gong <shunhua.gong@happy-seed.com>
2020-07-24 16:50:34 +0800

    另一个同事修改了master

---
+++ test/src/components/Index.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-07-24 16:23:47
+ * @LastEditTime: 2020-07-24 16:48:28
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \test\test\src\components\Index.vue
+--> 
+<template>
+    <div>
+        这是我新增的页面
+    </div>
+</template>
+<script>
+export default {
+    name: "Index"
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
